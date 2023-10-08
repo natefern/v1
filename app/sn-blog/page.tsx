2fcc53a7081e0290eb940c771c3f9aa70d03fc23
natefern <nfernconsultive@gmail.com>
2023-10-07 19:32:45 -0700

    Update page.tsx

--- app/sn-blog/page.tsx
+++ app/sn-blog/page.tsx
@@ -1,10 +1,11 @@
+'use client'
 import React from 'react'
 
 const NewUserPage = () => {
     return (
         <>
             <h1>NewUserPage</h1>
-            <iframe src="https://scribehow.com/embed/Servicenow_Workflow__SttV4tl2S-iuupar25XxGQ" width="100%" height="640" allowfullscreen frameborder="0"></iframe>
+            <iframe src="https://scribehow.com/embed/Servicenow_Workflow__SttV4tl2S-iuupar25XxGQ" width="100%" height="640"></iframe>
         </>
 
 
