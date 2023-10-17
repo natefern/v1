cf5519a5fd5e11f435db8264ae3f9078f2499ff4
natefern <nfernconsultive@gmail.com>
2023-10-16 20:00:54 -0700

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,15 +2,13 @@
 import React from 'react'
 
 const BlogPage = () => {
+
     return (
         <>
             <div className='h-screen px-0 py-6 md:p-10'>
                 <iframe src="https://quizlet.com/720928847/learn/embed?i=3h8qp8&x=1jj1" height="100%" width="100%"></iframe>
             </div>
-
         </>
-
-
     )
 }
 
